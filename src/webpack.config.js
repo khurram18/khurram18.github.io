b0e08107e0d6cbd7ba15f562b534e33a0286c85e
Khurram Shehzad <khurram.pak522@gmail.com>
2026-08-07 17:16:36 +0500

    chore: upgrade dev toolchain to fix vulnerabilities (webpack, loaders, css-minimizer, sass)

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   watch: true,
   // https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new CssMinimizerPlugin({})],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
